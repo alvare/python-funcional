e7a58d6414378f10c61d2c7968cdfbd2296577ea
ealvarez <ealvarez@msa.com.ar>
2014-08-26 17:03:44 -0300

    variable binding

--- either-monad.py
+++ either-monad.py
@@ -1,4 +1,5 @@
-#from functools import reduce
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import sys
 from pymonad.Either import Right, Left
 from pymonad.Maybe import Just, Nothing
@@ -39,8 +40,16 @@ def user_is_tecnico(tecnicos, user):
         return Left('Usuario no es técnico')
 
 @curry
-def get_name(user):
+def get_nombre(user):
     return Right(user['name'])
+
+@curry
+def get_permisos(permisos, user):
+    if user['username'] in permisos:
+        return Right(permisos[user['username']])
+    else:
+        return Left('No tiene permisos.')
+
 
 users = [{'username': 'chancho', 'password': '444', 'name': 'Chanchito'},
          {'username': 'pedro', 'password': '123', 'name': 'Pedro Gomez'},
@@ -48,12 +57,16 @@ users = [{'username': 'chancho', 'password': '444', 'name': 'Chanchito'},
 
 tecnicos = ['pedro', 'loly']
 
+permisos = {'pedro': ['archivar', 'borrar']}
+
 req = {'username': sys.argv[1], 'password': sys.argv[2]}
 
 val = request_contains(['username', 'password'], req) >>\
       user_exists(users) >>\
       user_authenticates(users) >>\
-      user_is_tecnico(tecnicos) >>\
-      get_name
+      user_is_tecnico(tecnicos) >> (lambda user:
+      get_nombre(user) >> (lambda nombre:
+      get_permisos(permisos, user) >> (lambda permisos:
+      Right("El usuario {} tiene estos permisos: {}".format(nombre, ', '.join(permisos))))))
 
 print(val)
